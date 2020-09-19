b7c8a1f23ff0ae232a66f2c54cd5771c9a4476e6
Summer Duke <esrduke@gmail.com>
2020-09-19 10:49:29 -0500

    firebase auth setup

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,15 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
+import authData from './helpers/data/authData';
+import auth from './components/auth/auth';
+import mynavbar from './components/mynavbar/mynavbar'
 
 const init = () => {
-  console.warn('HELLO!');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  mynavbar.logoutEvent();
 };
 
 init();
